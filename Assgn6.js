9e762eab0a3380b682296fb941779c55f7397947
Awourrobberrt <sophierobberrt@gmail.com>
2021-03-28 17:36:45 +0300

    Assgn6

---
+++ Assgn6.js
@@ -0,0 +1,16 @@
+var kiosk={
+    fruits:["apple"],
+    fruit2:["mango"],
+    set fruit(fruit){
+        this.fruit2=this.fruit2
+
+    },
+    get fruit(){
+        return this.fruit2
+
+    },
+
+}
+console.log(kiosk.fruit2);
+
+
